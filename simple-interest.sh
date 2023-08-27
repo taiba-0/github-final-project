9e79b1c5059ed24ceb6745f3486cb826a644f0f5
Taiba Aslam <99859215+taiba-0@users.noreply.github.com>
2023-08-27 17:44:20 +0500

    simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # taiba-0
    # Input:
    # p, principal amount
    # t, time period in years
